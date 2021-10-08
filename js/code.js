11543ff0b105db3b0654166b3c779bf45839fa9a
arnaubalart <arnaubalart@yahoo.com>
2021-10-08 19:51:16 +0200

    coger valores del form

--- js/code.js
+++ js/code.js
@@ -1,3 +1,11 @@
 function calculo() {
-    alert("hellow");
+    // alert("hellow");
+    let totalCuenta = document.getElementById("total").value;
+    let servicio = document.getElementById("servicio").value;
+    let personas = document.getElementById("personas").value;
+    console.log('Total: ' + totalCuenta + ', servicio: ' + servicio + 'personas: ' + personas);
+    /*
+        let servicio = document.getElementsByName('servicio')[0].value;
+        let personas = document.getElementsByClassName('personas')[0].value;
+*/
 }
